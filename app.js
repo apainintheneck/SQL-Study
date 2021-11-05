56b6fb3d3d932b9cab212f16a0327f62a362cfc8
apainintheneck <apainintheneck@gmail.com>
2021-11-05 14:32:04 -0700

    Added additional api routes to app.js
    Get /chapters : Returns everything from chapters table
    Get /pages :
    	Action = page : Returns single page based upon id
    	Action = chapter : Returns all pages in chapter by chapter_id
    
    Post /admin/chapters/add : Form information to add new chapter
    Post /admin/chapters/edit : Form information to edit chapter
    Post /admin/pages/add : Form information to add new page
    Post /admin/pages/edit : Form submission info to edit page
    
    To Do:
    Create .ejs pages for admin section.
    Render pages off of post requests depending upon their success.
    Create /admin page and route.

--- app.js
+++ app.js
@@ -9,12 +9,90 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+
 //routes
 app.get("/", function(req, res){
-  res.render("index");
+    res.render("index");
 });
+
+
+//apis
+app.get("/chapters", function(req, res){
+    let sql = "SELECT * FROM chapters";
+
+    pool.query(sql, function (err, rows, fields) {
+        if (err) throw err;
+        // console.log(rows); //testing
+        res.send(rows);//sends data
+    });
+});
+
+app.post("/admin/chapters/add", function(req, res){
+    let sql = "INSERT INTO chapters (title, description) VALUES (?,?)";
+    let sqlParams = [req.body.title, req.body.description];
+
+    pool.query(sql, sqlParams, function(err, rows, fields) {
+        if (err) throw err;
+        // console.log(rows); //testing
+        res.send(rows);//sends data
+    });
+});
+
+app.post("/admin/chapters/edit", function(req, res){
+    let sql = "UPDATE chapters SET title = ?, description = ? WHERE id = ?";
+    let sqlParams = [req.body.title, req.body.description, req.body.id];
+
+    pool.query(sql, sqlParams, function(err, rows, fields) {
+        if (err) throw err;
+        // console.log(rows); //testing
+        res.send(rows);//sends data
+    });
+});
+
+app.get("/pages", function(req, res){
+    let sql;
+    let sqlParams = [];
+
+    switch(req.query.action){
+        case "page":        sql = "SELECT * FROM pages WHERE id = ?";
+                          sqlParams.push(req.query.id);
+                          break;
+        case "chapter":   sql = "SELECT * FROM pages WHERE chapter_id = ?";
+                          sqlParams.push(req.query.id);
+                          break;
+    }
+
+    pool.query(sql, sqlParams, function (err, rows, fields) {
+        if (err) throw err;
+        // console.log(rows); //testing
+        res.send(rows);//sends data
+    });
+});
+
+app.post("/admin/pages/add", function(req, res){
+   let sql = "INSERT INTO pages (chapter_id, title, body) VALUES (?,?,?)";
+   let sqlParams = [req.body.chapter_id, req.body.title, req.body.body];
+
+   pool.query(sql, sqlParams, function(err, rows, fields) {
+      if (err) throw err;
+      // console.log(rows); //testing
+      res.send(rows);//sends data
+   });
+});
+
+app.post("/admin/pages/edit", function(req, res){
+    let sql = "UPDATE pages SET title = ?, body = ? WHERE id = ?";
+    let sqlParams = [req.body.title, req.body.body, req.body.id];
+
+    pool.query(sql, sqlParams, function(err, rows, fields) {
+        if (err) throw err;
+        // console.log(rows); //testing
+        res.send(rows);//sends data
+    });
+});
+
 
 //starting server
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
-  console.log("Express server is running...");
+    console.log("Express server is running...");
 });
