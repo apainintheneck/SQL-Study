616b25b8853c8e688a7fee8ce3eb037e9c43dc57
Harsandeep <harssingh@csumb.edu>
2021-11-27 01:03:36 -0800

    Logout button addition

--- app.js
+++ app.js
@@ -69,6 +69,11 @@ app.get("/login", function(req, res){
     res.render("login");
 });
 
+app.get("/logout", function(req, res){
+    req.logOut()
+    res.redirect("/")
+});
+
 app.get("/", function(req, res){
     res.render("index");
 });// "/"
